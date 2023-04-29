1be1541f81777629aa9baeb56aeda0908ca5d7d7
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-04-30 01:55:29 +0800

    Add password encryption to the seeder

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,23 +1,24 @@
 import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcrypt';
 import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function main() {
   const mockUserCount = 100;
   for (let i = 1; i <= mockUserCount; i++) {
-    // const first_name = faker.name.firstName();
-    // const last_name = faker.name.lastName();
-    // const email =
-    //   `${first_name}_${last_name}@${faker.internet.domainName()}`.toLowerCase();
-    // await prisma.user.create({
-    //   data: {
-    //     first_name,
-    //     last_name,
-    //     email,
-    //     password: faker.internet.password(8),
-    //     avatar: faker.internet.avatar(),
-    //   },
-    // });
+    const first_name = faker.name.firstName();
+    const last_name = faker.name.lastName();
+    const email =
+      `${first_name}_${last_name}@${faker.internet.domainName()}`.toLowerCase();
+    await prisma.user.create({
+      data: {
+        first_name,
+        last_name,
+        email,
+        password_hash: await hash(faker.internet.password(8), 10),
+        avatar: faker.internet.avatar(),
+      },
+    });
   }
 }
 main()
