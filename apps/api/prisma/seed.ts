4fe905f6c1a1f5dbae21b7522e59de563712d6fc
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-04-29 22:17:35 +0800

    Add database seeder

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+const prisma = new PrismaClient();
+
+async function main() {
+  const mockUserCount = 100;
+  for (let i = 1; i <= mockUserCount; i++) {
+    const first_name = faker.name.firstName();
+    const last_name = faker.name.lastName();
+    const email =
+      `${first_name}_${last_name}@${faker.internet.domainName()}`.toLowerCase();
+
+    await prisma.user.create({
+      data: {
+        first_name,
+        last_name,
+        email,
+        password: faker.internet.password(8),
+        avatar: faker.internet.avatar(),
+      },
+    });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
