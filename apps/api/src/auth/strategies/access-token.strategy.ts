182b4b10b79f77a58f94b545fc6e4f4758824222
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-04-30 08:46:08 +0800

    Define token strategies

---
+++ apps/api/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,18 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.ACCESS_TOKEN_KEY,
+    });
+  }
+
+  async validate(payload: any) {
+    return payload;
+  }
+}
---
+++ apps/api/src/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.REFRESH_TOKEN_KEY,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(req: Request, payload: any) {
+    const refreshToken = req.get('authorization').replace('Bearer', '').trim();
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
